2adc401897011380983a8d2f9be785f62d5b74e5
Iñaki Úcar <i.ucar@enchufa2.es>
2015-03-12 18:47:26 +0100

    vqm

---
+++ test/vqm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding=UTF8
+## This file is part of VideoTester
+## See http://video-tester.googlecode.com for more information
+## Copyright 2015 Iñaki Úcar <i.ucar86@gmail.com>
+## This program is published under a GPLv3 license
+
+# A simple script to make use of the PSNR and SSIM methods
+
+import sys
+import os.path
+from VideoTester.video import YUVvideo
+from VideoTester.measures.vq import PSNR, SSIM
+
+if len(sys.argv) != 4:
+  print 'Usage: ./vqm.py <degraded_yuv> <original_yuv> <width>x<height>'
+  sys.exit()
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+width, height = map(int, sys.argv[3].split('x'))
+
+if not os.path.isfile(file1) or 
+   not os.path.isfile(file2):
+  raise IOError, 'bad file(s)'
+
+def save_to_file(name, res):
+  with open(name, 'w') as f:
+    for frame, dB in zip(res['axes'][0], res['axes'][1]):
+      f.write('%i %i\n' % (frame, dB))
+
+print "Loading videos..."
+rawdata = {
+  'received' : YUVvideo(file1, (width, height)), 
+  'original' : YUVvideo(file2, (width, height))
+}
+
+psnr = PSNR((None, rawdata, None, None))
+ssim = SSIM((None, rawdata, None, None))
+
+print "Calculating PSNR..."
+res = psnr.calculate()
+save_to_file('result_psnr.dat', res)
+  
+print "Calculating SSIM..."
+res = ssim.calculate()
+save_to_file('result_ssim.dat', res)
